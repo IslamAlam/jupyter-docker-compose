f6a1b273861529a5baf114e32d75840d6ef41179
Islam Mansour <islam.alam.aerospace@gmail.com>
2019-10-02 11:13:30 +0200

    Update install-demos.sh

--- service.jupyter/demo/install-demos.sh
+++ service.jupyter/demo/install-demos.sh
@@ -13,6 +13,21 @@ conda env create -f tensorflow_1.4.yml
 source activate tensorflow_1_4
 python -m ipykernel install --user --name "tensorflow_1_4" --display-name "Tensorflow 1.4"
 source deactivate
+
+#conda env create -f tensorflow_1.4_gpu.yml
+#source activate tensorflow_1_4_gpu
+#python -m ipykernel install --user --name "tensorflow_1_4_gpu" --display-name "Tensorflow GPU 1.4"
+#source deactivate
+
+conda env create -f tensorflow_1.14.yml
+source activate tensorflow_1_14
+python -m ipykernel install --user --name "tensorflow_1_14" --display-name "Tensorflow 1.14"
+source deactivate
+
+#conda env create -f tensorflow_1.14_gpu.yml
+#source activate tensorflow_1_14_gpu
+#python -m ipykernel install --user --name "tensorflow_1_14_gpu" --display-name "Tensorflow GPU 1.14"
+#source deactivate
 # popd
 # cd /home/jovyan/work
 # git clone https://github.com/IslamAlam/DSAC.git 
