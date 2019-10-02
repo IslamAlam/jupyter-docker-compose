be61a38246aab950ba41f7f644fc9bb13870b2df
Islam Mansour <islam.alam.aerospace@gmail.com>
2019-10-02 11:02:27 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+docker-compose up --build
+# docker-compose up -d --build
+# find / -xdev 2>/dev/null -name "tensorflow_1_4"
+# nohup ./build.sh >> my.log 2>>&1 &
+# nohup ./build.sh >> my.log 2>>1 &
